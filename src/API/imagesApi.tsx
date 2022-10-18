7000c1957ddb1180eb5b592b6ee2662b249ef0b7
a.urbanite <a.urbanite@gmail.com>
2022-10-18 22:37:46 +0200

    mad imageFetch work

--- src/API/imagesApi.tsx
+++ src/API/imagesApi.tsx
@@ -1,5 +1,10 @@
 const  fetchImages = async () => {
-  const res = await fetch('https://unsplash.com/photos/random')
+  const res = await fetch('https://api.unsplash.com/photos/random', {
+    // mode: 'cors',
+    headers: {
+      'Authorization': 'Client-ID q0QKQFpa2DffR2h8Akz1ITaEzNTQi8c1gRyxewil-K4'
+    }
+  })
   const imgArr = res.json()
   // console.log(imgArr)
   return imgArr
